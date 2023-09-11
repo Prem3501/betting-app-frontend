e2a266639f58b3c253892a1c750f98b6e6df2d9b
ak220193 <mail2meak22frcrio@gmail.com>
2023-09-11 12:43:42 +0530

    sportsScreen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import ResponsibleGambling from "./pages/ResponsibleGambling";
 import Termsandcondition from "./pages/Termsandcondition";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SportsScreen from "./pages/SportsScreen";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/ResponsibleGambling" element={<ResponsibleGambling />} />
         <Route path="/Termsandcondition" element={<Termsandcondition />} />
         <Route path="/About" element={<About />} />
+        <Route path="/SportsScreen" element={<SportsScreen />} />
+        
       </Routes>
     </Router>
   );
---
+++ src/pages/SportsScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './SportsScreen.css';
+
+import Sidebar from "../component/sidebar"; 
+import Navbar from "../component/Navbar";
+import Heroimagebanner from "../component/Heroimagebanner";
+
+
+
+
+function SportsScreen() {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+       <Heroimagebanner /> 
+      
+       
+       
+       
+      
+      
+      
+    </div>
+  );
+}
+
+export default SportsScreen;
