2f886f41eba082c96d30d7521c4d19302c24c937
ak220193 <mail2meak22frcrio@gmail.com>
2023-10-01 00:07:19 +0530

    too much bug issues

--- src/pages/kyc.js
+++ src/pages/kyc.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidebar from "../component/sidebar";
+import Sidebar from "../component/Sidebar-jami";
 import Navbarall from "../component/Navbar-all";
 import Heroimagebanner from "../component/Heroimagebanner";
 import CenterContainerKYC from "../component/CenterContainerKYC";
-
 import Rightsmallbanner from "../component/Rightsmallbanner";
 import Rightlongbanner from "../component/Rightlongbanner";
+import Footer from "../component/Footerall";
 import "./KYC.css";
 // import Rightlongbanner from '../component/Rightlongbanner';
 
@@ -13,11 +13,10 @@ function Kyc() {
   return (
     <div>
       <Navbarall />
-
-      <Rightsmallbanner />
-      <Heroimagebanner />
-      <CenterContainerKYC />
       <Sidebar />
+      <Heroimagebanner />
+      <Rightsmallbanner />
+      <CenterContainerKYC />
       <Rightlongbanner />
     </div>
   );
