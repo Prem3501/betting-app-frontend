c55b1d931aec8d8065c9e67e45f6abba0754d5de
ak220193 <mail2meak22frcrio@gmail.com>
2023-09-30 22:13:14 +0530

    footer changes done

--- src/component/Footerall.js
+++ src/component/Footerall.js
@@ -1,10 +1,26 @@
 import React from "react";
-import WhatsAppButton from "./subfooter";
 import logo from "../images/5_1.png";
 import payment from "../images/payment.png";
+import { BsWhatsapp, BsTwitter } from "react-icons/bs";
 import "./Footerall.css";
 
 function Footer() {
+  const footerLogoStyle = {
+    display: "flex",
+    alignContent: "space-between",
+    flexWrap: "wrap",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    marginTop: "-21pc",
+    marginRight: "7pc",
+    flexDirection: "row",
+  };
+
+  const paymentImageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
   return (
     <div className="footer-container" id="footer-all">
       <div className="row mt-3">
@@ -17,29 +33,29 @@ function Footer() {
 
       <div className="row">
         <div className="col-md-4">
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={footerLogoStyle} id="footer-logo">
             <img
               id="payment"
               alt=""
               src={logo}
               width="100"
               height="100"
-              className="d-inline-block align-top"
+              className="d-inline-block align-top d-flex justify-center"
             />
-            <h5 style={{ marginLeft: "2px", marginBottom: "0" }}>BETZHUB</h5>
+            <h5 style={{ marginLeft: "2px", marginBottom: "0px" }}>BETZHUB</h5>
           </div>
         </div>
-        <div className="col-md-4 mt-4">
+        <div class="col-md-4" style={{ marginTop: "-16pc" }}>
           <h4>100% safe & Instant payments</h4>
         </div>
-        <div className="col-md-4 mt-4">
+        <div class="col-md-4" style={{ marginTop: "-16pc" }}>
           <h4>Accepted Modes of Payments</h4>
         </div>
       </div>
       <br />
 
       <div className="row">
-        <div className="col-md-4">
+        <div class="col-md-4" style={{ marginTop: "-14pc" }}>
           Betzhub.co is Asia’s one of the biggest gaming platforms with over 350
           games and sports available.
           <br />
@@ -55,23 +71,39 @@ function Footer() {
           <br />
           <br />
           <br />
-          <WhatsAppButton phoneNumber="1234567890"> </WhatsAppButton>
+          <div className="row">
+            <div className="col-md-1" id="so-icon">
+              <BsWhatsapp style={{ width: "30px", height: "30px" }} />
+            </div>
+            <div className="col-lg m-2">WhatsApp</div>
+            <div className="col-md-1" id="so-icon">
+              <BsTwitter
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  color: "black",
+                  marginLeft: "2px",
+                }}
+              />
+            </div>
+            <div className="col-lg m-2">Twitter</div>
+          </div>
           <br />
-          &copy; Copyright 2010 - 2023 Betzhub
+          &copy; Copyright 2023 Betzhub
         </div>
-        <div className="col-md-4 mt-1">
+        <div class="col-md-4" style={{ marginTop: "-12pc" }}>
           You can make payments and receive earnings instantly via your UPI ID -
           so you can be sure that your money is safe and secure. All UPI
           platforms are accepted.
         </div>
-        <div className="col-md-4">
+        <div class="col-md-4" style={{ marginTop: "-12pc" }}>
           <img
             alt=""
             src={payment}
-            width="120%"
-            height="30%"
-            className="d-inline-block align-top"
-            style={{ marginLeft: "-35px" }}
+            width="110%"
+            height="20%"
+            class="d-inline-block align-top"
+            style={{ marginLeft: "-25px" }}
           />
           <br />
           <br />
--- src/pages/Sports.js
+++ src/pages/Sports.js
@@ -17,6 +17,7 @@ function Sports() {
 
       <Test />
       <Rightlongbanner />
+
       <Footerall />
     </div>
   );
