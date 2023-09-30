49197a01485c35cc043852846e4f2bfa5225513e
ak220193 <mail2meak22frcrio@gmail.com>
2023-09-30 23:50:20 +0530

    corrections

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,28 @@
 // AboutUs.js
 import React from "react";
-import Sidebar from "../component/sidebar";
+import Sidebarj from "../component/Sidebar-jami";
 import Navbarall from "../component/Navbar-all";
 import Heroimagebanner from "../component/Heroimagebanner";
 import CenterContainer from "../component/CenterContainer";
 import Rightsmallbanner from "../component/Rightsmallbanner";
 import Rightlongbanner from "../component/Rightlongbanner";
+import Footer from "../component/Footerall";
 import "./About.css";
 
 function AboutUs() {
   return (
     <div>
       <Navbarall />
-      <Rightsmallbanner />
       <Heroimagebanner />
-      <Sidebar />
-      <Rightlongbanner />
+      <Sidebarj />
+      <Rightsmallbanner />
       <CenterContainer />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Rightlongbanner />
+      <Footer />
     </div>
   );
 }
