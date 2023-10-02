0e5e8893e0e30475cb6adef39512f7fd23138559
ak220193 <mail2meak22frcrio@gmail.com>
2023-10-02 22:01:42 +0530

    account-page-coorections

--- src/component/Account.js
+++ src/component/Account.js
@@ -11,22 +11,23 @@ const Account = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container" id="Account-container">
       <h2 className="account">Account Statement</h2>
       <div className="row">
-        <div className="entry-control">
+        <div className="entry-control" id="Show">
           <label htmlFor="entriesDropdown">Show </label>
+
+          <span className="entries-label m-1">Entries</span>
           <select
             id="entriesDropdown"
             className="custom-dropdown"
             value={selectedEntries}
             onChange={handleDropdownChange}
           >
-            {/* Options for the dropdown */}
+            {/* Add options for the dropdown here */}
           </select>
-          <span className="entries-label">Entries</span>
         </div>
-        <div className="search-bar">
+        <div className="search-bar" id="search-bar">
           <input type="text" placeholder="Search" className="search-input" />
           {/* Add your search icon here */}
           <i className="fa fa-search search-icon" aria-hidden="true"></i>
@@ -35,29 +36,29 @@ const Account = () => {
       {/* Render the selected number of entries here */}
       <div className="button-row">
         {/* Six buttons */}
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Date <i className="fas fa-sort-up"></i>
           <i className="fas fa-sort-down"></i>
         </button>
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Type <i className="fas fa-sort-up"></i>
-          <i className="fas fa-sort-down"></i>
+          <i className="fas fa-sort-down" style={{ marginLeft: "-11px" }}></i>
         </button>
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Description <i className="fas fa-sort-up"></i>
-          <i className="fas fa-sort-down"></i>
+          <i className="fas fa-sort-down" style={{ marginLeft: "-11px" }}></i>
         </button>
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Dr <i className="fas fa-sort-up"></i>
-          <i className="fas fa-sort-down"></i>
+          <i className="fas fa-sort-down" style={{ marginLeft: "-11px" }}></i>
         </button>
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Cr <i className="fas fa-sort-up"></i>
-          <i className="fas fa-sort-down"></i>
+          <i className="fas fa-sort-down" style={{ marginLeft: "-11px" }}></i>
         </button>
-        <button className="page-button">
+        <button className="page-button" id="page-button">
           Balance <i className="fas fa-sort-up"></i>
-          <i className="fas fa-sort-down"></i>
+          <i className="fas fa-sort-down" style={{ marginLeft: "-11px" }}></i>
         </button>
       </div>
       <div className="horizontal-line"></div>
@@ -65,7 +66,7 @@ const Account = () => {
       <div className="horizontal-line"></div>
       <div className="pagination">
         <div className="entries-info">0 to 0 of 0 entries</div>
-        <div className="page-navigation">
+        <div className="page-navigation mb-4">
           <button className="prev-button">Previous</button>
           <button className="next-button">Next</button>
         </div>
